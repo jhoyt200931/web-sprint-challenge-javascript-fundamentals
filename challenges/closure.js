c294d9ae63f472f9a12e2cc0e565881b91691d7e
Johnathan Hoyt <johnathanhoyt@Gabriellas-MacBook-Pro.local>
2020-06-12 22:00:37 -0400

    finished closure and prototypes

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,17 @@ myFunction();
 
 // Explanation: 
 
+// The nested function can acess the variable internal because the nested function has closure, allowing it to reach outside itself to get the information it needs to work, and the variable internal is declared within the function's lexical environment.
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+let summation = function(num) {
+  let sum = 0;
+  for(let i = 0; i < num + 1 ; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(summation(4));
